1523b2d162149b1e9a1521622df48cce7091fb50
Abhishek Singhal <abhisinghal112001@gmail.com>
2023-08-22 15:26:10 +0530

    img repace images

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import { useAuth } from "@/context/AuthContext";
 
 export default function Home() {
   const { user,clubData } = useAuth();
+  console.log(process.env.key)
   return (
     <main className="center min-h-screen w-screen flex-col p-5 ">
       {user && clubData && <NewPost image = {clubData.photoURL} uid = {user.uid} />}
